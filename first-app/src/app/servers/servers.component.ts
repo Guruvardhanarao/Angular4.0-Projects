69482057cc842b836f9d63f7835c78c22ba89aa0
Guruvardhanarao <guruvardhanaraopanidam@gmail.com>
2017-06-05 09:30:41 +0530

    cpdde added

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -7,10 +7,19 @@ import {Component} from '@angular/core'
 })
 export class ServersComponent {
     buttonDisable: Boolean = false;
+    serverCreate: String = "Server Not Created";
+    serverName = "test server";
     constructor() {
         setTimeout(() => {
             this.buttonDisable = true;
         },3000)
     }
 
+    addServer() {
+        return this.serverCreate = "One Server Created.Name is " + this.serverName;
+    }
+
+onUpdateServerName(event){
+    this.serverName = event.target.value;
+}
 }
