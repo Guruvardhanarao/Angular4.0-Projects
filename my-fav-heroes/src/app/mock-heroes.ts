7534e7ef58ab53d109087b4e97104baa8f9aec38
Guruvardhanarao <guruvardhanaraopanidam@gmail.com>
2017-10-13 10:14:12 +0530

    new project added

---
+++ my-fav-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,45 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {
+     id: 10,
+     name: 'N. Taraka RamaRao'
+   },
+   {
+     id: 11,
+     name: 'a. NageswaraRao'
+   },
+   {
+     id: 12,
+     name: 'G. Krishna'
+   },
+   {
+     id: 13,
+     name: 'M. Chiramjeevi'
+   },
+   {
+     id: 14,
+     name: 'N. Balakrishna'
+   },
+   {
+     id: 15,
+     name: 'N. Young Tiger'
+   },
+   {
+     id: 16,
+     name: 'A. Arjune'
+   },
+   {
+     id: 17,
+     name: 'M. Ram Charan'
+   },
+   {
+     id: 18,
+     name: 'G. Mahesh babu'
+   },
+   {
+     id: 19,
+     name: 'Venkatesh'
+   },
+  ];
+  
