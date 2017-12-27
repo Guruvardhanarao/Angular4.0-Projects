d06cb96d687af7b25c4819498cf3e1bdb223d952
Guruvardhanarao <guruvardhanaraopanidam@gmail.com>
2017-12-27 16:29:46 +0530

    template driven forms example

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  firstname: string = "ganesha";
+
+  public formSubmit(data: any): void {
+    console.log("data", data);
+  }
 }
